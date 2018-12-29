8801cb32ea2fbc616f0ddaab66cc9c9b10fe4718
RyuNen344 <s1100633@outlook.com>
2018-12-29 23:37:08 +0900

    first commit of this project

--- CarthageSwiftPra/ViewController.swift
+++ CarthageSwiftPra/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        SVProgressHUD.show()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
