a895c138cadf6d27edf74ab751cf6e64b5097795
RyuNen344 <s1100633@outlook.com>
2018-12-29 23:10:43 +0900

    Initial Commit

---
+++ CarthageSwiftPra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageSwiftPra
+//
+//  Created by Bunjiro on 2018/12/29.
+//  Copyright © 2018 Bunjiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
